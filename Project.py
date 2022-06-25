5f4ffc8904da16365a86c84b68887d379f94db6e
abdullahbyrmv <abdullah.bayramov@inbox.ru>
2022-06-25 14:55:36 +0400

    first project

---
+++ Project.py
@@ -0,0 +1,6 @@
+print("Welcome to Name Generator")
+
+city_name = input("Please type the name of city you grew up in : \n")
+pet_name = input("Please type the name of your pet : \n")
+
+print("Your band name could be " + city_name + " " + pet_name)
